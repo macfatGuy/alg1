486b502058d1142865e9e7c534437d9552e293a8
Влад <@DESKTOP-KIIK14P>
2024-11-26 23:44:15 +0300

    Добавьте файлы проекта.

---
+++ alg1/alg1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+
+int countElementsGreaterThan(const std::vector<int>& arr, int threshold) {
+    int left = 0;
+    int right = arr.size() - 1;
+    int result = -1; 
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] <= threshold) {
+            left = mid + 1;
+        }
+        else {
+            result = mid; 
+            right = mid - 1; 
+        }
+    }
+
+   
+    if (result == -1) return 0;
+
+    return arr.size() - result;
+}
+
+int main() {
+    std::vector<int> sortedArray = { 14, 16, 19, 32, 32, 32, 56, 69, 72 };
+    int threshold;
+
+    std::cout << "Введите точку отсчёта: ";
+    std::cin >> threshold;
+
+    int count = countElementsGreaterThan(sortedArray, threshold);
+    std::cout << "Количество элементов в массиве больших, чем " << threshold << ": " << count << std::endl;
+
+    return 0;
+}
+
